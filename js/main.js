7378aa2f399f580d6f573494de9736149a758543
Faramanta <katuxa@yandex.ru>
2018-11-19 16:11:49 +0500

    gems

--- js/main.js
+++ js/main.js
@@ -31,5 +31,26 @@ $(document).ready(function() {
         $('.header-bot__nav-link-open').removeClass('active');
     });
 
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        vertical: true
+    });
+
+
+
+
 });
 
